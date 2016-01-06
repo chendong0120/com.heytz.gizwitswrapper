2fbf207511ed0b2fa43621e694c8dd7ea87691d0
陈东东 <kevinchen@infapp.com>
2016-01-06 18:46:51 +0800

    添加控制方法和获取列表方法,升级sdk

--- www/gwsdkwrapper.js
+++ www/gwsdkwrapper.js
@@ -1,7 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.setDeviceWifi = function (ssid, wifikey, appId, success, error) {
-    exec(success, error, "gwsdkwrapper", "setDeviceWifi", [ssid, wifikey, appId]);
+exports.setDeviceWifi = function (appid,productKey,wifiSSID,wifiKey, success, error) {
+    exec(success, error, "gwsdkwrapper", "setDeviceWifi", [appid,productKey,wifiSSID,wifiKey]);
+};
+exports.getDeviceList = function (appid,productKey,uid,token, success, error) {
+    exec(success, error, "gwsdkwrapper", "getDeviceList", [appid,productKey,uid,token]);
+};
+exports.deviceControl = function (appid,productKey,uid,token,mac,value, success, error) {
+    exec(success, error, "gwsdkwrapper", "deviceControl", [appid,productKey,uid,token,mac,value]);
 };
 
 exports.deAlloc = function(success, error){
